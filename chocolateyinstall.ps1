7d5c7151f5dd8a9b6c02e4b9577fcc9bc7c17501
Sathish <42292676+satheeshsoft@users.noreply.github.com>
2019-04-06 02:09:46 +0530

    Add files via upload

---
+++ chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'loginTimer.exe'
+$packageName = 'loginTimer'
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  file         = $fileLocation
+  softwareName  = 'loginTimer'
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyInstallPackage @packageArgs
