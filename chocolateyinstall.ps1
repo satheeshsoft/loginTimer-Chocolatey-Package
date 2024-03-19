1c7b600967cd16dbd126a6429988e4921483e257
Sathish <42292676+satheeshsoft@users.noreply.github.com>
2024-03-20 03:03:36 +0530

    Update chocolateyinstall.ps1

--- chocolateyinstall.ps1
+++ chocolateyinstall.ps1
@@ -1,13 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'loginTimer.exe'
-$packageName = 'loginTimer'
-$packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  file         = $fileLocation
-  softwareName  = 'loginTimer'
-  validExitCodes= @(0, 3010, 1641)
 }
 Install-ChocolateyInstallPackage @packageArgs
