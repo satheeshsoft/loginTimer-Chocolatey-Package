96ec3c4c7fb98928a88fc6a732268b8db9338921
Sathish <42292676+satheeshsoft@users.noreply.github.com>
2024-03-20 03:05:20 +0530

    Update chocolateyuninstall.ps1

--- chocolateyuninstall.ps1
+++ chocolateyuninstall.ps1
@@ -2,16 +2,10 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     $packageArgs['file'] = ''
-    } else {      
+    } else {
     }
-
-    Uninstall-ChocolateyPackage @packageArgs
+   Uninstall-ChocolateyPackage @packageArgs
   }
 } elseif ($key.Count -eq 0) {
-  Write-Warning "$packageName has already been uninstalled by other means."
-} elseif ($key.Count -gt 1) {
-  Write-Warning "$($key.Count) matches found!"
-  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-  Write-Warning "Please alert package maintainer the following keys were matched:"
   $key | % {Write-Warning "- $($_.DisplayName)"}
 }
